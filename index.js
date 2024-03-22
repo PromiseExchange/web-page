da666efa6ab3e42ad5e0daf3776946e3c5e2cf76
PromiseExchange <114079064+PromiseExchange@users.noreply.github.com>
2024-03-22 17:59:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-function toggleMenu() {
-    var menuLinks = document.getElementById("menuLinks");
-    var menuIcon = document.getElementById("menuIcon");
-    var menu = document.getElementById("menu");
-  
-    if (menuLinks.classList.contains("show")) {
-      menuLinks.classList.remove("show");
-      menuIcon.style.display = "block";
-      menu.style.backgroundColor = "transparent";
-      document.body.style.overflow = "auto";
-    } else {
-      menuLinks.classList.add("show");
-      menuIcon.style.display = "none";
-      menu.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
-      document.body.style.overflow = "hidden";
-    }
-  }
-  
-  function hideMenu() {
-    var menuLinks = document.getElementById("menuLinks");
-    var menuIcon = document.getElementById("menuIcon");
-    var menu = document.getElementById("menu");
-  
-    menuLinks.classList.remove("show");
-    menuIcon.style.display = "block";
-    menu.style.backgroundColor = "transparent";
-    document.body.style.overflow = "auto";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-
-  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
